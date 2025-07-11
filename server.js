661dba0b0ea9d10ff0a62682e8ef021418d923b7
NicolasPietroDEV <nicolas.lpietro@gmail.com>
2025-07-11 17:29:29 -0300

    cors policy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5501",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
